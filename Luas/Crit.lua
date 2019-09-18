69ecdb0e0d0729319db927d94029d9c2abb48362
Joshua Dooley <DooleyJoshuaM@JohnDeere.com>
2019-09-18 03:42:09 -0500

    Split lua up into multiple files.

--- Luas/Crit.lua
+++ Luas/Crit.lua
@@ -1,13 +1,26 @@
-local playerGUID = UnitGUID("player")
-local combatLogFrame = CreateFrame("Frame")
-combatLogFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-combatLogFrame:SetScript("OnEvent", function(self, event) 
-	self:OnEvent(event, CombatLogGetCurrentEventInfo())
-end)
-SendSystemMessage("Crit Commander loaded")
+-- Creating namespace
+local CritCommander, NS = Crit
 
 
-function combatLogFrame:OnEvent(event, ...)
+-- Setting defaults for locals
+local soundDelay = .5
+local soundChannel = "Dialog"
+
+
+local function register()
+    local playerGUID = UnitGUID("player")
+    
+    local combatLogFrame = CreateFrame("Frame")
+    combatLogFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+    combatLogFrame:SetScript("OnEvent", function(self, event) 
+        self:OnEvent(event, CombatLogGetCurrentEventInfo())
+    end)
+    
+    SendSystemMessage("Crit Commander has been loaded.")
+end
+
+
+local function combatLogFrame:OnEvent(event, ...)
 	local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
 	local spellId, spellName, spellSchool
 	local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
@@ -21,10 +34,17 @@ function combatLogFrame:OnEvent(event, ...)
     end
 	
 	if critical and sourceGUID == playerGUID then
-		C_Timer.After(.5, playSound)
+		C_Timer.After(soundDelay, playSound)
 	end
 end
 
-function playSound()
-	PlaySoundFile("Interface\\AddOns\\CritCommander\\Sounds\\wow1.mp3", "Dialog")
+
+local function playSound()
+	PlaySoundFile("Interface\\AddOns\\CritCommander\\Sounds\\wow1.mp3", soundChannel)
 end
+
+
+-- Adding to the namespace
+NS.Crit.Register = register
+NS.Crit.SoundDelay = soundDelay
+NS.Crit.SoundChannel = soundChannel
---
+++ Luas/Main.lua
@@ -0,0 +1,3 @@
+NS.Crit.Register()
+NS.Crit.SoundDelay = .5
+NS.Crit.SoundChannel = "Dialog" -- Master, SFX, Music, Ambience, Dialog
