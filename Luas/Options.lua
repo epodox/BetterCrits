7a9ed79f369e9bebadeda024e9ba28961b50d4f5
Joshua Dooley <DooleyJoshuaM@JohnDeere.com>
2019-09-20 06:54:58 -0500

    Continueing to work on options.

--- Luas/Options.lua
+++ Luas/Options.lua
@@ -1,5 +1,6 @@
 -- Register the Namespace.
-local CritCommander, NS = ...
+local _, NS = ...;
+NS.Options = {};
 
 
 -- Globals.
@@ -7,72 +8,73 @@ critCommander_variablesLoaded = false;
 
 
 -- Locals.
-local critCommanderConfig_DefaultSoundDelay = .5 -- Half a second.
-local critCommanderConfig_DefaultSoundChannel = "Dialog" -- Master, SFX, Music, Ambience, Dialog.
-local critCommanderConfig_DefaultDebug = true -- Make sure this is set to false for releases.
-local NS.critCommanderRealm = GetCVar("realmName")
-local NS.critCommanderChar = UnitName("player")
+local CritCommanderDB_DefaultSoundDelay = .5; -- Half a second.
+local CritCommanderDB_DefaultSoundChannel = "Dialog"; -- Master, SFX, Music, Ambience, Dialog.
+local CritCommanderDB_DefaultDebug = true; -- Make sure this is set to false for releases.
+local NS.Options.Realm = GetCVar("realmName");
+local NS.Options.Char = UnitName("player");
+
+
+-- Backing fucntions.
+-- Post messages if debug mode is turned on.
+function Options:SendOutput(msg)
+	if NS.Debug = true then
+		SendSystemMessage(msg);
+	end;
+end;
 
 
 -- Creates frame to listen for "VARIABLES_LOADED".
-local critCommanderFrame = CreateFrame("Frame")
-    critCommanderFrame:RegisterEvent("VARIABLES_LOADED")
-    critCommanderFrame:SetScript("OnEvent", function(self, event)
-        self:OnEvent(event)
-    end)
-end
+local varListenerFrame = CreateFrame("Frame")
+	varListenerFrame:RegisterEvent("VARIABLES_LOADED");
+	varListenerFrame:SetScript("OnEvent", function(self, event)
+        self:OnEvent(event);
+    end);
+end;
 
 
 -- Calls method once "VARIABLES_LOADED" has been confirmed.
-local function combatLogFrame:OnEvent(event)
+local function varListenerFrame:OnEvent(event)
     if (event == "VARIABLES_LOADED") then
-		critCommander_VARIABLES_LOADED()
-		SendOutput("Crit Commander - Variables loaded.")
-	end
-end
+		critCommander_VARIABLES_LOADED();
+		SendOutput("Crit Commander - Variables loaded.");
+	end;
+end;
 
 
 -- Method to load config
 local function critCommander_VARIABLES_LOADED()
 	-- Initialize our SavedVariable.
-	if (not critCommanderConfig) then 
-		critCommanderConfig = {}
-		SendOutput("Crit Commander - Creating config.")
-	end
-	if (not critCommanderConfig[critCommanderRealm]) then 
-		critCommanderConfig[critCommanderRealm] = {}
-		SendOutput("Crit Commander - Adding realm to config.")
-	end
-	if (not critCommanderConfig[critCommanderRealm][critCommanderChar]) then 
-		critCommanderConfig[critCommanderRealm][critCommanderChar] = {}
-		SendOutput("Crit Commander - Adding character to config.") 
-    end
+	if (not CritCommanderDB) then 
+		CritCommanderDB = {};
+		SendOutput("Crit Commander - Creating config.");
+	end;
+	if (not CritCommanderDB[critCommanderRealm]) then 
+		CritCommanderDB[critCommanderRealm] = {};
+		SendOutput("Crit Commander - Adding realm to config.");
+	end;
+	if (not CritCommanderDB[critCommanderRealm][critCommanderChar]) then 
+		CritCommanderDB[critCommanderRealm][critCommanderChar] = {};
+		SendOutput("Crit Commander - Adding character to config.");
+    end;
     
 
 	-- load each option, set default if not there.
-	if ( not critCommanderConfig[critCommanderRealm][critCommanderChar].SoundDelay ) then 
-		critCommanderConfig[critCommanderRealm][critCommanderChar].SoundDelay = critCommanderConfig_DefaultSoundDelay
-	end
-	if ( not critCommanderConfig[critCommanderRealm][critCommanderChar].SoundChannel ) then 
-		critCommanderConfig[critCommanderRealm][critCommanderChar].SoundChannel = critCommanderConfig_DefaultSoundChannel
-	end
-	if ( not critCommanderConfig[critCommanderRealm][critCommanderChar].Debug ) then 
-		critCommanderConfig[critCommanderRealm][critCommanderChar].Debug = critCommanderConfig_DefaultDebug
-	end
+	if ( not CritCommanderDB[critCommanderRealm][critCommanderChar].SoundDelay ) then 
+		CritCommanderDB[critCommanderRealm][critCommanderChar].SoundDelay = CritCommanderDB_DefaultSoundDelay;
+	end;
+	if ( not CritCommanderDB[critCommanderRealm][critCommanderChar].SoundChannel ) then 
+		CritCommanderDB[critCommanderRealm][critCommanderChar].SoundChannel = CritCommanderDB_DefaultSoundChannel;
+	end;
+	if ( not CritCommanderDB[critCommanderRealm][critCommanderChar].Debug ) then 
+		CritCommanderDB[critCommanderRealm][critCommanderChar].Debug = CritCommanderDB_DefaultDebug;
+	end;
     
 
 	-- Record that we have been loaded.
-    critCommander_variablesLoaded = true
-end
-
-
--- Post messages if debug mode is turned on.
-function SendOutput(msg)
-	if NS.Debug = true then
-		SendSystemMessage(msg)
-	end
+    critCommander_variablesLoaded = true;
 end
 
 
 -- Options.lua end.
-SendOutput("Crit Commander - Options.lua has been loaded.")
+SendOutput("Crit Commander - Options.lua has been loaded.");
